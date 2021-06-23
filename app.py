9e3930d7431b3caa4e24b42ec8d39df1037e4dfa
Bridgette <bridgette_lafaye@berkeley.edu>
2021-06-23 13:16:57 -0700

    More page formatting, added content to about page

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template, request
 from pull_city_data import plot_init_grid
 from plot_city_results import plot_venues
-from city_name_utils import get_city_state
+from city_name_utils import get_city_state, get_city_list
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template('home.html', page="home")
+    city_nicknames_list = get_city_list()
+    cities_list = [(get_city_state(nickname)[0], nickname) for nickname in city_nicknames_list]
+    return render_template('home.html', cities_list=cities_list, page="home")
 
 
 @app.route("/about")
@@ -30,9 +32,11 @@ def plot_full():
         city_nickname = 'sf'
     city, state = get_city_state(city_nickname)
     script, div = plot_venues(city, city_nickname, state)
-    return render_template('plot_full.html', script=script, div=div, page="full")
+    city_nicknames_list = get_city_list()
+    cities_list = [(get_city_state(nickname)[0], nickname) for nickname in city_nicknames_list]
+    return render_template('plot_full.html', script=script, div=div, cities_list=cities_list, page="full")
 
 
 if __name__ == '__main__':
-    app.run(port=33507)
-    # app.run(debug=True)
+    # app.run(port=33507)
+    app.run(debug=True)
--- city_name_utils.py
+++ city_name_utils.py
@@ -1,7 +1,21 @@
-def get_city_state(city_nickname):
+def city_dictionary():
     cities = {
         'sf': ('San Francisco', 'california'),
         'chicago': ('Chicago', 'illinois'),
         'nyc': ('New York', 'new_york')
     }
+    return cities
+
+
+def get_city_state(city_nickname):
+    cities = city_dictionary()
     return cities[city_nickname]
+
+
+def get_city_list():
+    cities = city_dictionary()
+    return cities.keys()
+
+
+if __name__ == '__main__':
+    pass
